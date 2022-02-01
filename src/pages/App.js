daa1c2ac1406b2fb5762b3dc827929cb838261f6
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-02-01 12:33:50 +0100

    [RWD-x] initial state

---
+++ src/pages/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import GlobalStyles from 'theme/GlobalStyles';
+
+const App = () => (
+  <div>
+    <GlobalStyles />
+    <p>hello</p>
+  </div>
+)
+
+export default App
